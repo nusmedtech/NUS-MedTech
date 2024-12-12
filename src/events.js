1bd7744634b56eb4c759fae99e6d2ed4319d794a
JiaYi-Gallium369 <seahjiayi369@gmail.com>
2024-12-12 23:50:16 +0800

    Improve styling for mobile

--- src/events.js
+++ src/events.js
@@ -1,26 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menu = document.querySelector('#mobile-menu');
-    const menuLinks = document.querySelector('.navbar__menu');
-  
-    menu.addEventListener('click', function() {
-      menu.classList.toggle('is-active');
-      menuLinks.classList.toggle('active');
-    });
-  
-    let slideIndex = 0;
-    showSlides();
-  
-    function showSlides() {
-      const slides = document.getElementsByClassName("slide");
-      for (let i = 0; i < slides.length; i++) {
-        slides[i].classList.remove("active");
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) {
-        slideIndex = 1;
-      }
-      slides[slideIndex - 1].classList.add("active");
-      setTimeout(showSlides, 3000);
-    }
+  const menu = document.querySelector('#mobile-menu');
+  const menuLinks = document.querySelector('.navbar__menu');
+
+  menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
   });
+});
+
   
